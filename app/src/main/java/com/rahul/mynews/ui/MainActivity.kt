a7bd9eaa0e6cc7bacf4a273037632f189a73c938
rahul_chhetri <immarshallchhetri@gmail.com>
2021-07-15 21:25:32 +1000

    Move MainActivity to UI package

--- app/src/main/java/com/rahul/mynews/ui/MainActivity.kt
+++ app/src/main/java/com/rahul/mynews/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rahul.mynews
+package com.rahul.mynews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rahul.mynews.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
