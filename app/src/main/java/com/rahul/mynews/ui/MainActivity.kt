b8bff03bd04a5389c1ba91b855abd0fa27069b7e
rahul_chhetri <immarshallchhetri@gmail.com>
2021-07-16 20:13:54 +1000

    Create blank about me fragment and navigate on menu clicked

--- app/src/main/java/com/rahul/mynews/ui/MainActivity.kt
+++ app/src/main/java/com/rahul/mynews/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -14,6 +15,9 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,9 +25,10 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
         setSupportActionBar(binding.toolbar)
     }
 
@@ -36,6 +41,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_info -> {
+                navController.navigate(R.id.aboutMeFragment)
                 true
             }
             android.R.id.home -> {
---
+++ app/src/main/java/com/rahul/mynews/ui/about/AboutMeFragment.kt
@@ -0,0 +1,22 @@
+package com.rahul.mynews.ui.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.rahul.mynews.databinding.FragmentAboutMeBinding
+
+class AboutMeFragment : Fragment() {
+    lateinit var mBinding: FragmentAboutMeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mBinding = FragmentAboutMeBinding.inflate(inflater, container, false)
+
+        return mBinding.root
+    }
+}
