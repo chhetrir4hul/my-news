a76c40ffbc2aef96fc6e8607d0ab89029ef7c61a
rahul_chhetri <immarshallchhetri@gmail.com>
2021-07-17 17:06:45 +1000

    Setup adapter to show news and weather view type items

--- app/src/main/java/com/rahul/mynews/ui/news/list/NewsListAdapter.kt
+++ app/src/main/java/com/rahul/mynews/ui/news/list/NewsListAdapter.kt
@@ -5,25 +5,58 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.rahul.mynews.data.Article
+import com.rahul.mynews.data.WeatherResponse
 import com.rahul.mynews.databinding.ItemNewsBinding
+import com.rahul.mynews.databinding.ItemWeatherBinding
 
 class NewsListAdapter(private val articleList: List<Article>) :
-    RecyclerView.Adapter<NewsListViewHolder>() {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsListViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val itemBinding = ItemNewsBinding.inflate(layoutInflater, parent, false)
-        return NewsListViewHolder(itemBinding)
+    companion object {
+        const val TYPE_WEATHER = 1
+        const val TYPE_NEWS = 2
     }
 
-    override fun onBindViewHolder(holder: NewsListViewHolder, position: Int) {
-        holder.bind(articleList[position])
-        holder.itemView.setOnClickListener {
-            val action =
-                NewsListFragmentDirections.actionNewsListFragmentToNewsDetailFragment(articleList[position])
-            it.findNavController().navigate(action)
+    private var weatherResponse: WeatherResponse? = null
+
+    fun setWeatherResponse(weatherResponse: WeatherResponse?) {
+        this.weatherResponse = weatherResponse
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return if (viewType == TYPE_WEATHER) {
+            val itemBinding = ItemWeatherBinding.inflate(layoutInflater, parent, false)
+            WeatherItemViewHolder(itemBinding)
+        } else {
+            val itemBinding = ItemNewsBinding.inflate(layoutInflater, parent, false)
+            NewsListViewHolder(itemBinding)
         }
     }
 
-    override fun getItemCount() = articleList.size
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is WeatherItemViewHolder) {
+            holder.bind(weatherResponse)
+        } else if (holder is NewsListViewHolder) {
+            val article = articleList[position - 1]
+            holder.bind(article)
+            holder.itemView.setOnClickListener {
+                val action =
+                    NewsListFragmentDirections.actionNewsListFragmentToNewsDetailFragment(
+                        article
+                    )
+                it.findNavController().navigate(action)
+            }
+        }
+
+    }
+
+    override fun getItemCount() = articleList.size + 1
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> TYPE_WEATHER
+            else -> TYPE_NEWS
+        }
+    }
 }
---
+++ app/src/main/java/com/rahul/mynews/ui/news/list/WeatherItemViewHolder.kt
@@ -0,0 +1,19 @@
+package com.rahul.mynews.ui.news.list
+
+import androidx.recyclerview.widget.RecyclerView
+import com.rahul.mynews.R
+import com.rahul.mynews.data.WeatherResponse
+import com.rahul.mynews.databinding.ItemWeatherBinding
+import com.rahul.mynews.util.GlideImageLoader
+
+class WeatherItemViewHolder(private val itemWeatherBinding: ItemWeatherBinding) :
+    RecyclerView.ViewHolder(itemWeatherBinding.root) {
+    fun bind(weather: WeatherResponse?) = with(itemWeatherBinding) {
+
+        weather?.let {
+            GlideImageLoader(ivWeatherIcon).loadWeatherIcon(weather.weather[0].icon)
+            tvTemperature.text = weather.main.temp.toString()
+            tvLocation.text = weather.name
+        } ?: run { tvLocation.text = itemView.context.getString(R.string.txt_loading) }
+    }
+}
