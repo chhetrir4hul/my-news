3fa747ed30c9d8b599ca16d3e73c26fb7e27c5fb
rahul_chhetri <immarshallchhetri@gmail.com>
2021-07-17 16:37:27 +1000

    Add method to load weather icon from openweathermap

--- app/src/main/java/com/rahul/mynews/util/GlideImageLoader.kt
+++ app/src/main/java/com/rahul/mynews/util/GlideImageLoader.kt
@@ -7,4 +7,8 @@ class GlideImageLoader(private val imageView: ImageView) {
     fun load(imageUrl: String?) {
         Glide.with(imageView.context).load(imageUrl).into(imageView)
     }
+
+    fun loadWeatherIcon(iconCode: String) {
+        load("https://openweathermap.org/img/w/$iconCode.png")
+    }
 }
